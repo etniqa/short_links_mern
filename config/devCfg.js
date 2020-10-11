a0c912d83551ebd9d84317ee7b820bc2120fe7ee
etniqa <etniqaaa@gmail.com>
2020-10-11 21:44:32 +0300

    change dev port to 5000 and prod to 3100

--- config/devCfg.js
+++ config/devCfg.js
@@ -1,8 +1,8 @@
 let config = {};
 
-config.port = 3100;
+config.port = 5000;
 config.jwtSecret = "I`m your lost agnets";
 config.mongoUri = "mongodb+srv://etniqa:centurion@cluster0.h3aol.azure.mongodb.net/app?retryWrites=true&w=majority";
-config.baseUrl = "http://localhost:3100";
+config.baseUrl = "http://localhost:5000";
 
 module.exports = config;
