63dc6b8b761fd6ad15b79b30fbdcd1c65f1b5f57
etniqa <etniqaaa@gmail.com>
2020-10-11 21:33:22 +0300

    change port in prod to 3100

--- config/prodCfg.js
+++ config/prodCfg.js
@@ -1,19 +1,8 @@
-/*
 let config = {};
 
-config.port = 100;
-config.jwtSecret = "I`m your lost agnets";
-config.mongoUri = "mongodb+srv://otherUser:password12346@cluster0.h3aol.azure.mongodb.net/app?retryWrites=true&w=majority";
-config.baseUrl = "http://localhost:100";
-
-module.exports = config;
-*/
-
-let config = {};
-
-config.port = 1300;
+config.port = 3100;
 config.jwtSecret = "I`m your lost agnets";
 config.mongoUri = "mongodb+srv://otherUser:password123456@cluster0.h3aol.azure.mongodb.net/app?retryWrites=true&w=majority";
-config.baseUrl = "http://localhost:1300";
+config.baseUrl = "http://localhost:3100";
 
 module.exports = config;
